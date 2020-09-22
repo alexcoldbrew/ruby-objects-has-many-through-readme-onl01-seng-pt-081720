a45faf368c8240b0b6c4c3e28ffea828d3d3a49f
Alex Sanchez <alexanderclaysanchez@gmail.com>
2020-09-22 01:32:53 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -15,6 +15,7 @@ class Customer
   end
   
   def new_meal(waiter, total, tip = 0)
+    # uses the initialize form Meal class, so the self is this customer instance
     Meal.new(waiter, self, total, tip)
   end
   
